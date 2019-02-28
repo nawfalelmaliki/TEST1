68a226dab29448cfd20af6ef887f4cbf6fbf0506
nawfalelmaliki <elmalikinawfal@gmail.com>
2019-02-28 21:55:05 +0100

    init project

---
+++ src/main/java/fr/nolito/PrintName.java
@@ -0,0 +1,22 @@
+package fr.nolito;
+import java.util.Scanner;
+import java.util.*;
+public class PrintName {
+
+    String nom ;
+    int nombre;
+    public  void set_nom(String  xx)
+    {
+        nom=xx;
+    }
+    public String get_nom()
+    {
+
+        return this.nom;
+    }
+    public static void main(String[] args){
+        PrintName p = new PrintName();
+        p.set_nom("nawfal");
+        System.out.println("votre nom est"+p.get_nom());
+    }
+}
